79599aa2c9d2659f2e3804b6003d221235ff718e
Analytics & Automation <136671732+Vikash-Data-Analytics@users.noreply.github.com>
2024-03-07 15:06:22 +0530

    Update views.py

--- Myapp/views.py
+++ Myapp/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render, redirect
-from Myapp.models import users
+from .models import users
 
 from django.views import View
 
